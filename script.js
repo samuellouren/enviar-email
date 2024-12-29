0911b8a7be15df58d4497c3e4ee31521f9fb255d
samuellouren <samuel.lourenco.sls@gmail.com>
2024-12-28 23:44:26 -0300

    subir o site para vercel

--- script.js
+++ script.js
@@ -2,23 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-
 const app = express();
 const PORT = 3000;
-
 // Configurações
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public')); 
-
 // Rota para enviar o e-mail
 app.post('/send-email', async (req, res) => {
     const { name, email, message } = req.body;
-
     if (!name || !email || !message) {
         return res.status(400).json({ error: 'Por favor, preencha todos os campos.' });
     }
-
     try {
         // Configuração do transporte
         const transporter = nodemailer.createTransport({
@@ -30,7 +25,6 @@ app.post('/send-email', async (req, res) => {
                 pass: process.env.PASSWORD
             } 
         });
-
         // Configuração do e-mail
         const mailOptions = {
             from: process.env.EMAIL,  
@@ -38,7 +32,6 @@ app.post('/send-email', async (req, res) => {
             subject: 'Mensagem Recebida!',
             text: `Olá, ${name}! recebemos sua mensagem: "${message}." Obrigado por testar!`
         };
-
         // Enviar e-mail
         await transporter.sendMail(mailOptions);
         res.status(200).json({ message: 'E-mail enviado com sucesso!' });
@@ -47,7 +40,6 @@ app.post('/send-email', async (req, res) => {
         res.status(500).json({ error: 'Erro ao enviar o e-mail.' });
     }
 });
-
 // Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
